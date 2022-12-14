03716d816f823f517d61430143816555801b09d9
Gislaine-DevOps <mgislaine2@yahoo.fr>
2022-12-14 04:26:55 +0000

    list file updated

--- app.java
+++ app.java
@@ -3,5 +3,4 @@ echo "Please enter your username"
 read username
 echo "Please enter youir password"
 read -s password
-echo "Welcome to the zelle application
-"
+echo "Welcome to the zelle application"
--- list.sh
+++ list.sh
@@ -3,3 +3,4 @@ Diane
 Tinan
 Gislaine
 Christian
+Eva
